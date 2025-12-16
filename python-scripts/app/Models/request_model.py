dd754567f4e406a2cc8fad6e9f0774af1227c174
Faiz Maula <fmaulaahmad@gmail.com>
2025-12-16 23:03:42 +0700

    first commit

--- python-scripts/app/Models/request_model.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-
-class VerificationRequest(BaseModel):
-    nama: str
-    kegiatan: str
-    tanggal: str  # string dd/mm/yyyy
-    penyelenggara: str
-    tanggal_variations: List[str] = None  # optional, bisa di-generate di backend
--- python-scripts/app/services/verification.py
+++
@@ -1,9 +0,0 @@
-def hitung_skor_akhir(match_score_kegiatan, match_score_nama, match_score_tanggal, S_search=20, S_gamma=10):
-    S_final = (((match_score_kegiatan + match_score_nama + match_score_tanggal) / 3) * 0.5) + S_search + S_gamma
-    if S_final >= 75:
-        status = "TERPERCAYA"
-    elif S_final >= 50:
-        status = "PERLU DITINJAU"
-    else:
-        status = "TIDAK TERPERCAYA"
-    return {"S_final": S_final, "status": status}
